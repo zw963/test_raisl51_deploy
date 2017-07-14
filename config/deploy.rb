a3412f66e495572bda772d98fa2ca4404c7cf191
Billy.Zheng <zw963@163.com>
2017-07-14 19:04:43 +0800

    update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :application, 'test_rails51_deploy'
 # 本地 git 示例: ssh://git@example.com:30000/~/me/my_repo.git
 # svn 示例: svn://myhost/myrepo/#{fetch(:branch)}
-set :repo_url, ''
+set :repo_url, 'git@github.com:zw963/test_raisl51_deploy'
 set :rails_env, -> { fetch(:stage) }
 set :rvm_ruby_version, "ruby-2.3.4@#{fetch(:application)}"
 set :keep_releases, 5
